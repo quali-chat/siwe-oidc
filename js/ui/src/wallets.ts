46d5f7a58ef667cb4a8a26074e3d60f9d8859883
Michele Franchi <michele.franchi933@gmail.com>
2024-12-09 13:04:49 +0100

    fix: add mobile binance wallet

--- js/ui/src/wallets.ts
+++ js/ui/src/wallets.ts
@@ -1,3 +1,7 @@
+import { CoreHelperUtil } from '@reown/appkit-core';
+
+console.log(CoreHelperUtil.isMobile());
+
 export const featuredWalletIds = [
 	// Metamask
 	'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
@@ -10,7 +14,7 @@ export const featuredWalletIds = [
 	// Safepal
 	'0b415a746fb9ee99cce155c2ceca0c6f6061b1dbca2d722b3ba16381d0562150',
 	// Binance
-	'8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4'
+	...(CoreHelperUtil.isMobile() ? ['8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4'] : [])
 ];
 
 export const excludedRdns = [
