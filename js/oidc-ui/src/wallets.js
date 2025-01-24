75ea6c379082feeb2059e7424469dd4fd71a4e11
Michele Franchi <michele.franchi933@gmail.com>
2025-01-24 16:25:54 +0100

    fix: wallets

--- js/oidc-ui/src/wallets.js
+++ js/oidc-ui/src/wallets.js
@@ -3,8 +3,8 @@ import { CoreHelperUtil } from "@reown/appkit-core";
 export const featuredWalletIds = [
   // Metamask
   "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
-  // Trust Wallet
-  "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
+  /*   // Trust Wallet
+  "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", */
   // Rainbow
   "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369",
   // Uniswap
