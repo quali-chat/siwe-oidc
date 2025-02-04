ede056560cc58e7926686c65c54eb7f7481db145
Michele Franchi <michele.franchi933@gmail.com>
2025-02-04 11:25:19 +0100

    fix: build path

--- js/oidc-ui/vite.config.ts
+++ js/oidc-ui/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: path.join(__dirname, "../../static"),
+  },
 });
